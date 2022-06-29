612fd659720217b0e05b13cbfe73a9e0b93eb94e
Lucca Macedo <89946241+MacedoMLucca@users.noreply.github.com>
2022-06-28 21:31:44 -0300

    Primeiro contato com REST API

---
+++ ApiRestExercicio/src/main/java/com/apirest/ApiRestExercicioApplication.java
@@ -0,0 +1,46 @@
+package com.apirest;
+
+import java.util.stream.LongStream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.apirest.entities.Contact;
+import com.apirest.repositories.ContactRepository;
+
+@SpringBootApplication
+public class ApiRestExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestExercicioApplication.class, args);
+	}
+
+	
+		
+	
+    @Bean
+    CommandLineRunner init(ContactRepository repository) {
+        return args -> {
+            repository.deleteAll();
+            LongStream.range(1, 11)
+                    .mapToObj(i -> {
+                        Contact c = new Contact();
+                        c.setName("Contact " + i);
+                        c.setEmail("contact" + i + "@email.com");
+                        c.setPhone("(111) 111-1111");
+                        return c;
+                    })
+                    .map(v -> repository.save(v))
+                    .forEach(System.out::println);
+        };
+    }
+	
+	
+	
+	
+	
+	
+
+}
